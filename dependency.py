decaf2e2c950b17435b2bcb6f1df65fc21fc7f89
Balachandar-Madasamy <balachandar.im@gmail.com>
2025-06-26 22:38:01 +0530

    feat : dependancy for telecom_predict.py file

---
+++ dependency.py
@@ -0,0 +1,47 @@
+internet_map = {
+    'No': 1,
+    'DSL': 0,
+    'Fiber Optic': 1
+}
+
+onlinesec = {
+    'No':0,
+    'Yes':2,
+    'No internet service':1
+}
+onlinebac ={
+    'No':0,
+    'Yes':2,
+    'No internet service':1
+}
+deviceprot ={
+    'No':0,
+    'Yes':2,
+    'No internet service':1
+}
+techsupp ={
+    'No':0,
+    'Yes':2,
+    'No internet service':1
+}
+streamtv ={
+    'No':0,
+    'Yes':2,
+    'No internet service':1
+}
+streammov ={
+    'No':0,
+    'Yes':2,
+    'No internet service':1
+}
+contrac = {
+    'Month-to-month':0,
+    'Two year':2,
+    'One year':1
+}
+pay_method={
+'Electronic check':2,
+'Mailed check':3,
+'Bank transfer (automatic)':0,
+'Credit card (automatic)':1
+}
